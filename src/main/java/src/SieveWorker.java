66beb28f76c349038dc718aed32678342f346fa7
Leifhaa <leifhaa@hotmail.com>
2022-03-14 22:13:54 +0100

    Currently working

--- src/main/java/src/SieveWorker.java
+++ src/main/java/src/SieveWorker.java
@@ -3,39 +3,35 @@ package src;
 public class SieveWorker implements Runnable {
     private final int readFrom;
     private final int readTo;
-    private final int n;
     private final int root;
     private int numOfPrimes;
     byte[] oddNumbers;
     int[] primes;
 
 
-
     public SieveWorker(int readFrom, int readTo, int[] primes, int n) {
         this.readFrom = readFrom;
         this.readTo = readTo;
-        this.root = Math.min((int)Math.sqrt(readTo), (int)Math.sqrt(n));;
-        this.n = n;
+        this.root = Math.min((int) Math.sqrt(readTo), (int) Math.sqrt(n));
         this.primes = primes;
         oddNumbers = new byte[(readTo - readFrom / 16) + 1];
     }
 
     @Override
     public void run() {
-        numOfPrimes = 1;
-        for (int i = 1; i < primes.length; i++){
-            if (primes[i] > root){
+        for (int i = 1; i < primes.length; i++) {
+            if (primes[i] > root) {
                 break;
             }
             //Cross out based on pre-known primes
             traverse(primes[i]);
-            numOfPrimes++;
         }
     }
 
     private void traverse(int prime) {
-        for (int i = prime*prime; i <= n; i += prime * 2)
-            mark(i);
+        for (int i = prime * prime; i <= readTo; i += prime * 2)
+            if (i >= readFrom)
+                mark(i);
     }
 
     private void mark(int num) {
@@ -52,23 +48,22 @@ public class SieveWorker implements Runnable {
     }
 
     public int[] collectPrimes() {
-
-        int start = (root % 2 == 0) ? root + 1 : root + 2;
+        int start = (readFrom % 2 == 0) ? readFrom + 1 : readFrom + 2;
 
         for (int i = start; i <= readTo; i += 2)
             if (isPrime(i))
                 numOfPrimes++;
 
-        int[] primes = new int[numOfPrimes];
 
-        primes[0] = 2;
-
-        int j = 1;
-
-        for (int i = 3; i <= readTo; i += 2)
+        int[] primes = new int[numOfPrimes];;
+        int j = 0;
+        for (int i = start; i <= readTo; i += 2)
             if (isPrime(i))
                 primes[j++] = i;
 
+        if (start == 1){
+            primes[0] = 2;
+        }
         return primes;
     }
 
--- src/test/java/TestSieveWorker.java
+++ src/test/java/TestSieveWorker.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import src.ParallelSieve;
 import src.SieveOfEratosthenes;
@@ -6,13 +7,42 @@ import src.SieveWorker;
 public class TestSieveWorker {
 
     @Test
-    public void TestTransition() {
+    public void TestSieveFirstChunk() {
         int n = 100;
         SieveOfEratosthenes sieve = new SieveOfEratosthenes(n);
         int[] primes = sieve.getPrimes();
         SieveWorker sw = new SieveWorker(0, 16, primes, n);
         sw.run();
-        int[] foo = sw.collectPrimes();
-
+        int[] results = sw.collectPrimes();
+        Assert.assertEquals(results.length, 6);
+        int[] expected = new int[]{2,3,5,7,11,13};
+        Assert.assertArrayEquals(results, expected);
     }
+
+
+    @Test
+    public void TestSieveSecondChunk() {
+        int n = 20000;
+        SieveOfEratosthenes sieve = new SieveOfEratosthenes((int)Math.sqrt(n));
+        int[] primes = sieve.getPrimes();
+        SieveWorker sw = new SieveWorker(16, 32, primes, 100000);
+        sw.run();
+        int[] results = sw.collectPrimes();
+        int[] expected = new int[]{17,19,23,29,31};
+        Assert.assertArrayEquals(results, expected);
+    }
+
+
+    @Test
+    public void TestSieveLast() {
+        int n = 200_000_000_0;
+        SieveOfEratosthenes sieve = new SieveOfEratosthenes((int)Math.sqrt(n));
+        int[] primes = sieve.getPrimes();
+        SieveWorker sw = new SieveWorker( 2, n, primes, n);
+        sw.run();
+        int[] results = sw.collectPrimes();
+        //int[] expected = new int[]{17,19,23,29,31};
+        //Assert.assertArrayEquals(results, expected);
+    }
+
 }
