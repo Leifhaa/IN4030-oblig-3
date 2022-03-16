3563fe7e0e3520378636c263cba863235abc07fe
Leifhaa <leifhaa@hotmail.com>
2022-03-16 02:01:42 +0100

    Fixes

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -26,19 +26,18 @@ public class Main {
 
         try {
             threads = Integer.parseInt(args[1]);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Second argument is not an integer");
             printInstructions();
             return;
         }
 
-        if (threads < 0){
+        if (threads < 0) {
             System.out.println("Threads argument can not be negative");
             printInstructions();
             return;
         }
-        if (threads == 0){
+        if (threads == 0) {
             threads = Runtime.getRuntime().availableProcessors();
         }
 
@@ -58,33 +57,111 @@ public class Main {
 
         if (mode == 0) {
             long start = System.nanoTime();
-            SieveOfEratosthenes sieve = new SieveOfEratosthenes(n);
-            int[] paraPrimes = sieve.getPrimes();
-            Factorization factorize = new Factorization(n, paraPrimes);
-            factorize.factorizeAll();
-            factorize.precode.writeFactors("Sequentual");
+            int[] paraPrimes = runSequentialSieve(n);
+            runSequentialFactorization(n, paraPrimes);
             double end = (double) (System.nanoTime() - start) / 1000000;
             System.out.println("Finished in: " + end + "ms");
-        }
-        else if (mode == 1){
+        } else if (mode == 1) {
             long start = System.nanoTime();
-            ParallelSieve sieve = new ParallelSieve(n, threads);
-            sieve.start();
-            int[] primes = sieve.collectPrimes();
-            ParallelFactorization para = new ParallelFactorization(n, threads, primes);
-            para.factorizeAll();
+            int[] primes = runParallelSievve(n, threads);
+            runParallelFactorization(n, threads, primes);
             double end = (double) (System.nanoTime() - start) / 1000000;
             System.out.println("Finished in: " + end + "ms");
-        }
-        else if (mode == 2){
-            runBenchmarks(n, threads);
+        } else if (mode == 2) {
+            runBenchmarks(threads);
         }
     }
 
-    private static void runBenchmarks(int n, int threads){
+    private static void runParallelFactorization(int n, int threads, int[] primes) {
+        ParallelFactorization para = new ParallelFactorization(n, threads, primes);
+        para.factorizeAll();
+    }
+
+    private static int[] runParallelSievve(int n, int threads) {
+        ParallelSieve sieve = new ParallelSieve(n, threads);
+        sieve.start();
+        return sieve.collectPrimes();
+    }
+
+    private static void runSequentialFactorization(int n, int[] paraPrimes) {
+        Factorization factorize = new Factorization(n, paraPrimes);
+        factorize.factorizeAll();
+        factorize.precode.writeFactors("Sequentual");
+    }
+
+    private static int[] runSequentialSieve(int n) {
+        SieveOfEratosthenes sieve = new SieveOfEratosthenes(n);
+        int[] paraPrimes = sieve.getPrimes();
+        return paraPrimes;
+    }
+
+    private static void runBenchmarks(int threads) {
+        System.out.println("Running benchmarks. This can take a while, please wait...");
+        int[] testNumbers = {2000000, 20000000, 200000000, 2000000000};
+
+        System.out.println("Starting benchmarking sieves...");
+        for (int testNumber : testNumbers) {
+            speedTestSieves(testNumber, threads);
+        }
+        System.out.println("Finished benchmarking sieves...");
+
+        System.out.println("Starting benchmarking factorization...");
+        for (int testNumber : testNumbers) {
+            speedTestFactorization(testNumber, threads);
+        }
+        System.out.println("Finished benchmarking factorization...");
 
     }
 
+    private static void speedTestFactorization(int testNumber, int threads) {
+        int totalRuns = 7;
+        int[] primes = runParallelSievve(testNumber, threads);
+
+        double[] seqFac = new double[totalRuns];
+        for (int i = 0; i < totalRuns; i++) {
+            long time = System.nanoTime();
+            runSequentialFactorization(testNumber, primes);
+            seqFac[i] = (System.nanoTime() - time) / 1000000.0;
+        }
+        double seqMedian = seqFac[(seqFac.length) / 2];
+        System.out.println("Sequential Factorization used median time " + seqMedian + " for n = " + testNumber);
+
+
+        double[] paraFac = new double[totalRuns];
+        for (int i = 0; i < totalRuns; i++) {
+            long time = System.nanoTime();
+            runParallelFactorization(testNumber, threads, primes);
+            paraFac[i] = (System.nanoTime() - time) / 1000000.0;
+        }
+        double parMedian = paraFac[(paraFac.length) / 2];
+        System.out.println("Paralell Factorization used median time " + parMedian + " for n = " + testNumber);
+        System.out.println("Speedup: " + seqMedian /  parMedian);
+    }
+
+
+    private static void speedTestSieves(int testNumber, int threads) {
+        int totalRuns = 7;
+        double[] seqSieve = new double[totalRuns];
+
+        for (int i = 0; i < totalRuns; i++) {
+            long time = System.nanoTime();
+            runSequentialSieve(testNumber);
+            seqSieve[i] = (System.nanoTime() - time) / 1000000.0;
+        }
+        double seqMedian = seqSieve[(seqSieve.length) / 2];
+        System.out.println("Sequential Sieve used median time " + seqMedian + " for n = " + testNumber);
+
+
+        double[] parSieve = new double[totalRuns];
+        for (int i = 0; i < totalRuns; i++) {
+            long time = System.nanoTime();
+            runParallelSievve(testNumber, threads);
+            parSieve[i] = (System.nanoTime() - time) / 1000000.0;
+        }
+        double parMedian = parSieve[(seqSieve.length) / 2];
+        System.out.println("Parallel Sieve used median time " + parMedian + " for n = " + testNumber);
+        System.out.println("Speedup: " + seqMedian /  parMedian);
+    }
 
 
     private static void printInstructions() {
